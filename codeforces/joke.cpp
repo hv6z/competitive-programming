483f43e4183639a641c9ff29951022f855eb1ebc
hv6z <havytran1111@gmail.com>
2026-08-24 00:19:23 +0700

    Add new slutions

---
+++ codeforces/joke.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string k, n, t;
+    cin >> k >> n >> t;
+
+    int kitu[26] = {};
+    for(int i = 0; i <= k.size(); i++){
+        int c = k[i] - 'A';
+        kitu[c]++;
+    }for(int i = 0 ; i < n.size(); i++){
+        int c = n[i] - 'A';
+        kitu[c]++;
+    }for(int i = 0; i < t.size(); i++){
+        int c =t[i] - 'A';
+        kitu[c]--;
+    } 
+    for(int i = 0; i < 26; i++){
+        if(kitu[i] != 0){
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+
+
+	return 0;
+ } 
