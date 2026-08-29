c2adebfbbcbeecc96c4a1d017c05354456b0cd9f
hv6z <havytran1111@gmail.com>
2026-08-29 19:31:28 +0700

    Add new slutions

--- codeforces/team.cpp
+++ codeforces/team.cpp
@@ -8,11 +8,11 @@ int main() {
     
     int n;
     cin >> n;
-    string s;
+    char s[3];
     int dem = 0;
 
     for(int i = 0; i < n; i++){
-        cin >> s;
+        cin >> s[0] >> s[1] >> s[2];
         if(s[0] == '1' && s[1] == '1' 
             || s[0] == '1' && s[2] == '1'
             || s[1] == '1' && s[2] == '1'){
