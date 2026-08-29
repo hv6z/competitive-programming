80d61ffe1036f54e5103aa7d3d289bf2c523e97b
hv6z <havytran1111@gmail.com>
2026-08-29 21:27:02 +0700

    Add new slutions

--- codeforces/158a.cpp
+++ codeforces/158a.cpp
@@ -13,7 +13,7 @@ int main() {
     int a[n];
     for(int i = 0; i < n; i++){
         cin >> a[i];
-    }for(int i = 0 ; i < n; i++){
+    }for(int i = 0; i < n; i++){
         if(a[i] >= a[k-1] && a[i] > 0){
             dem++;
         }
---
+++ codeforces/50a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int m, n;
+    cin >> m >> n;
+    int kq = (m *n) / 2;
+
+    cout << kq;
+	return 0;
+ } 
